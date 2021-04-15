13fd91b2b728d31d36039259276920a01d639d70
nyc0lai <nyc0lai@gmail.com>
2021-04-15 23:00:20 +0300

    corection point 5

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
 let overusedWords = ['really', 'very', 'basically'];
+const over =overusedWords;
 
 let unnecessaryWords = ['extremely', 'literally', 'actually' ];
 
@@ -8,16 +9,19 @@ let storyWords = story.split(' ');
 
 let betterWords = storyWords.filter( word => !unnecessaryWords.includes(word));
 
-// P5 varianta 1
-let arrOverusedWords = betterWords.filter( word => overusedWords.includes(word));
-let countOverusedWords = arrOverusedWords.length;
+let countW0 = 0;
+let countW1 = 0;
+let countW2 = 0;
+betterWords.forEach( word => {
+    if(word === over[0]){
+        countW0++;
+    }else if(word === over[1]){
+        countW1++
+    }else if(word === over[2]){
+        countW2++;
+    }
+});
 
-// P5 varianta 2
-let count = 0;
-for(let i=0; i< betterWords.length; i++){
-  if(overusedWords.includes(betterWords[i]))
-  count++;
-}
 
 let countSentence = 0;
 betterWords.forEach( word => {
@@ -28,7 +32,8 @@ betterWords.forEach( word => {
 const displayResult = () => {
     console.log(`The word count is ${betterWords.length}`);
     console.log(`The sentence count is ${countSentence}`);
-    console.log(`The number of times each overused word appears is ${count}`);
+    console.log(`The number of times each overused word appears is ${countW0+countW1+countW2} of which
+${over[0]} is ${countW0}\n${over[1]} is ${countW1}\n${over[2]} is ${countW2}`);
 };
 
 const arrToString = (arr) => {
@@ -36,7 +41,5 @@ const arrToString = (arr) => {
 };
 
 displayResult();
-console.log("");
+console.log();
 arrToString(betterWords);
-
-
